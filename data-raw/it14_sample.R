85914360c04e3e1e1dc0bca1c9cc0584d21772b4
JosepER <jespasareig@gmail.com>
2023-06-24 16:18:29 +0200

    create sample files in rda

--- data-raw/it14_sample.R
+++ data-raw/it14_sample.R
@@ -6,5 +6,15 @@ it14ip <- dplyr::as_tibble(haven::read_dta(
   "data-raw/it14ip.dta"
 ))
 
-usethis::use_data(it14ih, it14ip, overwrite = TRUE,
+it14ip <- dplyr::select(it14ip, hid:pid)
+
+it14wh <- dplyr::as_tibble(haven::read_dta(
+  "data-raw/it14wh.dta"
+))
+
+it14wp <- dplyr::as_tibble(haven::read_dta(
+  "data-raw/it14wp.dta"
+))
+
+usethis::use_data(it14ih, it14ip, it14wh, it14wp, overwrite = TRUE,
     internal=FALSE)
--- data-raw/us16_sample.R
+++ data-raw/us16_sample.R
@@ -6,5 +6,7 @@ us16ip <- dplyr::as_tibble(haven::read_dta(
   "data-raw/us16ip.dta"
 ))
 
+us16ip <- dplyr::select(us16ip, hid:pwgta)
+
 usethis::use_data(us16ih, us16ip, overwrite = TRUE,
     internal=FALSE)
