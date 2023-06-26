5fa38ba7b8dcdb230e561824d59630334a1f27f9
JosepER <jespasareig@gmail.com>
2023-06-26 14:21:13 +0200

    modify dashboard defaults

--- data-raw/dashboard.R
+++ data-raw/dashboard.R
@@ -1,6 +1,9 @@
-lis_dashboard_items <- c("D11R", "D12R", "B3GR", "D41R",
-    "D42R", "D45R", "D62R", "D5P", "D61P", "B6GR",
-    "K1R") 
+# Objects to reproduce LIS methodology in the Compare.it dashboard: https://comparability.lisdatacenter.org/shiny/comparability/
+
+lis_dashboard_items <- c("NFD11R", "NFB3GR", "NFD4R", "NFD41R", "NFD42R",
+                  "NFD45R",
+                  "NFD62R",
+                  "NFD5P", "NFD61P", "NFD12R", "NFB6GR", "NFK1R") 
 
 lis_dashboard_countries <- c("AUS", "AUT",
                          "CAN", "CHN", "DEU", "LTU", "LUX",
@@ -14,7 +17,7 @@ lis_dashboard_microdata_formulas <- list(
     D62R = "hpublic",
     `D5P+D61P-D12R` = "hxitsc + hxptax",
     `D61P-D12R` = "hxscont",
-    `dhi_B6G-K1R` = "hi11 + hi13 + hi12 + hicapital + hpublic - hxitsc - hxptax")
+    `B6GR-K1R` = "hi11 + hi13 + hi12 + hicapital + hpublic - hxitsc - hxptax")
 
 lis_dashboard_na_formulas <- list(
     D11R = "!is.na(D11R_S14) ~ D11R_S14 , !is.na(D11R_S14_S15) ~ D11R_S14_S15 , !is.na(D11R_S1) ~ D11R_S1",
@@ -24,5 +27,5 @@ lis_dashboard_na_formulas <- list(
     D61P = "!is.na(D61P_S14) ~ D61P_S14 , !is.na(D61P_S14_S15) ~ D61P_S14_S15 , !is.na(D61P_S1) ~ D61P_S1"
 )
 
-usethis::use_data(lis_dashboard_items, dashboard_countries, dashboard_formulas,
+usethis::use_data(lis_dashboard_items, lis_dashboard_countries, lis_dashboard_microdata_formulas, lis_dashboard_na_formulas,
     overwrite = TRUE, internal = FALSE)
