b7d36c9c3ba2882eb7f77cd050523f5466df221c
JosepER <jespasareig@gmail.com>
2023-06-26 22:24:34 +0200

    fix typo

--- tests/testthat/test-download_na.R
+++ tests/testthat/test-download_na.R
@@ -1,11 +1,5 @@
 library(testthat)
 
-# Test that function accepts ISO2 or ISO3 codes
-test_that("Function accepts ISO2 or ISO3 codes", {
-    expect_silent(download_na_oecd(1967, countries = c("AU", "AT", "BE")))
-    expect_silent(download_na_oecd(1967, countries = c("AUS", "AUT", "BEL")))
-})
-
 # Test that function does not accept codes of length other than 2 or 3
 test_that("Function does not accept codes of length other than 2 or 3", {
     expect_error(download_na_oecd(1967, countries = c("A", "B", "C")),
